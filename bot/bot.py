9802643ff7f16dc4a6c36e0fc3f5c25644361343
SansDoge <54908811+SansDoge@users.noreply.github.com>
2022-01-13 11:13:27 -0800

    Initialize in new work environment

--- bot/bot.py
+++ bot/bot.py
@@ -1,16 +1,13 @@
 import os
-
 import discord
 from dotenv import load_dotenv
-
 load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN')
 
-client = discord.Client()
+class Bot(discord.Client):
+    
+    async def on_ready(self):
+        print(f'{client.user.name} has connected to Discord!')
 
+client = Bot()
+client.run(os.getenv('BOT_TOKEN'))
 
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-
-client.run(TOKEN)
